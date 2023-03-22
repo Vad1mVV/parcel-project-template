f0e21871f4bbee80d3427781028e57b5ef403b45
Vad1mVV <107924193+Vad1mVV@users.noreply.github.com>
2023-03-22 19:11:26 +0200

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,19 @@
+const refs = {
+  form: document.querySelector('.form'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(e.srcElement)
+}
+
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
