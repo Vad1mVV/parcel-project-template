0b3ceef0a8a3065f8f2e8e171446aba123b611a6
Vad1mVV <107924193+Vad1mVV@users.noreply.github.com>
2023-03-22 19:53:44 +0200

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,19 +7,35 @@ refs.form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(e) {
   e.preventDefault();
 
-  console.log(e.srcElement)
+  let delayVal = Number(e.target.elements.delay.value);
+  const stepVal = Number(e.target.elements.step.value);
+  const amountVal = Number(e.target.elements.amount.value);
+
+  refs.form.reset();
+
+  for (let i = 1; i <= amountVal; i += 1) {
+    createPromise(i, delayVal)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    })
+    delayVal += stepVal;
+  }
 }
-
-
-
-
+  
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
 }
 
